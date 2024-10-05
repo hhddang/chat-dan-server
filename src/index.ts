d6d2c2fa36844fc14ec6e5297e929435d5e0d13c
hhddang <dangtramthien@gmail.com>
2024-10-05 21:37:02 +0700

    v241005.1

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,20 @@ const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 const PORT = 4000;
 
+enum SocketEvent {
+  JOIN_APP = "JOIN_APP",
+  USER_ID = "USER_ID",
+  SEND_MESSAGE = "SEND_MESSAGE",
+  RECEIVE_MESSAGE = "RECEIVE_MESSAGE",
+}
+
 io.on("connection", (socket) => {
-  socket.emit("user-id", socket.id);
+  socket.on(SocketEvent.JOIN_APP, () => {
+    socket.emit(SocketEvent.USER_ID, socket.id);
+  });
 
-  socket.on("join", (userId: string) => {
-    socket.broadcast.emit("new-user", userId);
-
-    socket.on("leave", () => {
-      socket.broadcast.emit("user-leave", userId);
-    });
+  socket.on(SocketEvent.SEND_MESSAGE, (message: string) => {
+    socket.broadcast.emit(SocketEvent.RECEIVE_MESSAGE, message);
   });
 });
 
