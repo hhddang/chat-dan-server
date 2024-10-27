1727e961b72d2f12595628781f26f43dc6ac8c72
hhddang <dangtramthien@gmail.com>
2024-10-27 21:36:09 +0700

    v241027.1

--- src/index.ts
+++ src/index.ts
@@ -8,76 +8,28 @@ const io = new Server(server, { cors: { origin: "*" } });
 const PORT = 4000;
 
 enum SocketEvent {
-  JOIN_APP = "JOIN_APP",
-  CREATE_ROOM = "CREATE_ROOM",
+  SOCKET_ID = "SOCKET_ID",
   JOIN_ROOM = "JOIN_ROOM",
   LEAVE_ROOM = "LEAVE_ROOM",
   MESSAGE = "MESSAGE",
 }
 
-let roomIds: string[] = [];
-const createRoomId = () => {
-  let roomId = "";
-  const length = 20;
-  const characters = "0123456789";
-
-  for (let i = 0; i < length; i++) {
-    roomId += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-
-  return roomId;
-};
-
 io.on("connection", (socket) => {
-  const userId = socket.id;
-
-  socket.on(SocketEvent.JOIN_APP, () => {
-    socket.join("global");
-    socket.emit(SocketEvent.JOIN_APP, userId);
-    socket.to("global").emit(SocketEvent.JOIN_APP, userId);
-  });
-
-  socket.on(SocketEvent.CREATE_ROOM, () => {
-    const roomId = createRoomId();
-    roomIds.push(roomId);
-
-    socket.join(roomId);
-    socket.emit(SocketEvent.CREATE_ROOM, roomId);
+  socket.on(SocketEvent.SOCKET_ID, () => {
+    socket.emit(SocketEvent.SOCKET_ID, socket.id);
   });
 
   socket.on(SocketEvent.JOIN_ROOM, (roomId: string) => {
-    if (roomIds.includes(roomId)) {
-      socket.join(roomId);
-      socket.to(roomId).emit(SocketEvent.JOIN_ROOM, userId);
-
-      // Emit userId to confirm
-      socket.emit(SocketEvent.JOIN_ROOM, userId);
-    }
+    socket.join(roomId);
   });
 
   socket.on(SocketEvent.LEAVE_ROOM, (roomId: string) => {
-    if (roomIds.includes(roomId)) {
-      socket.leave(roomId);
-      socket.to(roomId).emit(SocketEvent.LEAVE_ROOM, userId);
-
-      // Emit userId to confirm
-      socket.emit(SocketEvent.LEAVE_ROOM, userId);
-    }
+    socket.leave(roomId);
   });
 
-  socket.on(SocketEvent.MESSAGE, (message: string, roomId?: string) => {
-    if (roomId) {
-      socket.to(roomId).emit(SocketEvent.MESSAGE, message, userId);
-    } else {
-      socket.broadcast.emit(SocketEvent.MESSAGE, message, userId);
-    }
+  socket.on(SocketEvent.MESSAGE, (message: unknown, roomId: string) => {
+    socket.to(roomId).emit(SocketEvent.MESSAGE, message);
   });
-});
-
-io.of("/").adapter.on("delete-room", (roomId: string) => {
-  if (roomIds.includes(roomId)) {
-    roomIds = roomIds.filter((id) => id !== roomId);
-  }
 });
 
 server.listen(PORT, () => {
