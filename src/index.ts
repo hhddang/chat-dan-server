02230dff31aad86c433cb407fc689bcde0102054
hhddang <dangtramthien@gmail.com>
2024-11-03 22:42:55 +0700

    v241103.1

--- src/index.ts
+++ src/index.ts
@@ -6,29 +6,39 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 const PORT = 4000;
+let currentRoomId: string | null = null;
 
 enum SocketEvent {
-  SOCKET_ID = "SOCKET_ID",
+  CONNECT = "CONNECT",
+  DISCONNECT = "DISCONNECT",
   JOIN_ROOM = "JOIN_ROOM",
   LEAVE_ROOM = "LEAVE_ROOM",
   DATA = "DATA",
 }
 
 io.on("connection", (socket) => {
-  socket.on(SocketEvent.SOCKET_ID, () => {
-    socket.emit(SocketEvent.SOCKET_ID, socket.id);
+  socket.on("connect", () => {
+    socket.emit(SocketEvent.CONNECT, socket.id);
   });
 
   socket.on(SocketEvent.JOIN_ROOM, (roomId: string) => {
+    currentRoomId = roomId;
     socket.join(roomId);
   });
 
   socket.on(SocketEvent.LEAVE_ROOM, (roomId: string) => {
+    currentRoomId = null;
     socket.leave(roomId);
   });
 
-  socket.on(SocketEvent.DATA, (roomId: string, key: string, data: unknown) => {
-    socket.to(roomId).emit(SocketEvent.DATA, key, data);
+  socket.on(SocketEvent.DATA, (id: string, key: string, data: unknown) => {
+    socket.to(id).emit(SocketEvent.DATA, key, data);
+  });
+
+  socket.on("disconnect", () => {
+    if (currentRoomId) {
+      socket.to(currentRoomId).emit(SocketEvent.DISCONNECT, socket.id);
+    }
   });
 });
 
