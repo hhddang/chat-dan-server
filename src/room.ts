fd64dfcf50741aa4d536eca6774a175a26816fd1
hhddang <dangtramthien@gmail.com>
2024-11-21 21:44:42 +0700

    v241121.1

--- src/room.ts
+++ src/room.ts
@@ -1,40 +1,74 @@
+type UserId = string;
+type RoomId = string;
 type Room = {
   capacity: number;
-  members: string[];
+  userIds: UserId[];
+};
+
+type UserManager = {
+  [userId: UserId]: RoomId | null;
 };
 
 type RoomManager = {
-  [roomId: string]: Room;
+  [roomId: RoomId]: Room;
 };
 
-const rooms: RoomManager = {
+// ------------------
+
+let userManager: UserManager = {};
+
+let roomManager: RoomManager = {
   global: {
     capacity: 100,
-    members: [],
+    userIds: [],
   },
 };
 
-delete rooms["global"];
-
 const joinApp = (): string => {
-  const userId = Math.random() * 100 + "";
+  const userId = (Math.random() * 100 + "") as UserId;
+  userManager[userId] = null;
   return userId;
 };
 
-const joinRoom = (roomId: string, user: string): boolean => {
-  const room = rooms[roomId];
-  if (room.members.length >= room.capacity) {
-    return false;
+const leaveApp = (userId: UserId): boolean => {
+  delete userManager[userId];
+  return true;
+};
+
+const joinRoom = (roomId: RoomId, userId: UserId): boolean => {
+  const isExisted = roomId in roomManager;
+  if (isExisted) {
+    const room = roomManager[roomId];
+    const isOverCapacity = room.userIds.length >= room.capacity;
+    if (isOverCapacity) {
+      return false;
+    } else {
+      room.userIds.push(userId);
+      userManager[userId] = roomId;
+      return true;
+    }
   } else {
-    room.members.push(user);
+    roomManager[roomId] = {
+      capacity: 100, // handle this ....
+      userIds: [userId],
+    };
+    userManager[userId] = roomId;
     return true;
   }
 };
 
-const leaveRoom = (roomId: string, user: string): boolean => {
-  const room = rooms[roomId];
-  room.members = room.members.filter((member) => member !== user);
-  return true;
-};
+const leaveRoom = (userId: UserId): boolean => {
+  const roomId = userManager[userId];
+  if (roomId) {
+    const room = roomManager[roomId];
+    room.userIds = room.userIds.filter((id) => id !== userId);
+    if (room.userIds.length <= 0) {
+      delete roomManager[roomId];
+    }
+    userManager[userId] = null;
 
-const;
+    return true;
+  } else {
+    return false;
+  }
+};
