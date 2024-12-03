2e7bc3c3f16b98c38c7dd680d0ee7c594952f9f0
hhddang <dangtramthien@gmail.com>
2024-12-03 22:02:09 +0700

    v241203.1

--- src/index.ts
+++ src/index.ts
@@ -21,64 +21,92 @@ enum SocketEvent {
 
 io.on("connection", (socket) => {
   let userId: string;
+  let userName: string = "test123";
+  let currentRoomId: string | null = null;
 
-  socket.on(SocketEvent.CONNECT, (userName?: string) => {
-    userId = manager.addUser(userName);
+  socket.on(SocketEvent.CONNECT, (name?: string) => {
+    userId = manager.addUser(name);
     socket.emit(SocketEvent.CONNECT, userId);
+    // userName = name
   });
 
-  socket.on(SocketEvent.JOIN_ROOM, (roomType: string, roomId: string) => {
-    const status = manager.joinRoom(roomType, roomId, userId);
+  socket.on(SocketEvent.JOIN_ROOM, (roomType: string, roomId) => {
+    // userId = manager.addUser(userName);
+    const status = true;
+    socket.join(roomId);
     socket.emit(SocketEvent.JOIN_ROOM, status);
-    socket.to(roomId).emit(SocketEvent.ADD_USER, userId);
+    socket.to(roomId).emit(SocketEvent.ADD_USER, { id: userId, name: userName });
+    currentRoomId = roomId;
   });
 
   socket.on(SocketEvent.LEAVE_ROOM, (roomId: string) => {
-    manager.leaveRoom(roomId, userId);
-    socket.to(roomId).emit(SocketEvent.REMOVE_USER, userId);
+    socket.leave(roomId);
+    currentRoomId = null;
   });
 
-  socket.on(SocketEvent.GET_USERS, () => {
-    const roomId = manager.getRoomId(userId);
-
-    if (roomId) {
-      const room = manager.socket.emit(SocketEvent.GET_USERS, manager.getUSers);
-    }
-
-    socket.on(SocketEvent.LEAVE_ROOM, () => {
-      const roomId = "";
-      socket.emit(SocketEvent.REMOVE_USER, userId);
-      socket.leave(roomId);
-    });
-  });
-
-  socket.on("disconnect", () => {
-    const roomId = manager.getRoomId(userId);
-    manager.disconnect(userId);
-    if (roomId) {
-      socket.to(roomId).emit(SocketEvent.REMOVE_USER, socket.id);
+  socket.onAny((event, data) => {
+    if (!(event in Object.values(SocketEvent))) {
+      socket.to(currentRoomId!).emit(event, data);
     }
   });
+
+  // socket.on(SocketEvent.CONNECT, (userName?: string) => {
+  //   userId = manager.addUser(userName);
+  //   socket.emit(SocketEvent.CONNECT, userId);
+  // });
+
+  // socket.on(SocketEvent.JOIN_ROOM, (roomType: string, roomId: string) => {
+  //   const status = manager.joinRoom(roomType, roomId, userId);
+  //   socket.emit(SocketEvent.JOIN_ROOM, status);
+  //   socket.to(roomId).emit(SocketEvent.ADD_USER, userId);
+  // });
+
+  // socket.on(SocketEvent.LEAVE_ROOM, (roomId: string) => {
+  //   manager.leaveRoom(roomId, userId);
+  //   socket.to(roomId).emit(SocketEvent.REMOVE_USER, userId);
+  // });
+
+  // socket.on(SocketEvent.GET_USERS, () => {
+  //   const roomId = manager.getRoomId(userId);
+
+  //   if (roomId) {
+  //     const room = manager.socket.emit(SocketEvent.GET_USERS, manager.getUSers);
+  //   }
+
+  //   socket.on(SocketEvent.LEAVE_ROOM, () => {
+  //     const roomId = "";
+  //     socket.emit(SocketEvent.REMOVE_USER, userId);
+  //     socket.leave(roomId);
+  //   });
+  // });
+
+  // socket.on("disconnect", () => {
+  //   const roomId = manager.getRoomId(userId);
+  //   manager.disconnect(userId);
+  //   if (roomId) {
+  //     socket.to(roomId).emit(SocketEvent.REMOVE_USER, socket.id);
+  //   }
+  // });
 
   // socket.on(SocketEvent.CREATE_ROOM, (roomType: RoomType) => {
   //   const roomId = manager.createRoom(roomType);
   //   socket.emit(SocketEvent.CREATE_ROOM, roomId);
   // });
 
-  socket.on(SocketEvent.JOIN_ROOM, (roomType: RoomType, roomId: string) => {
-    manager.joinRoom(roomId, userId);
-  });
+  // socket.on(SocketEvent.JOIN_ROOM, (roomType: RoomType, roomId: string) => {
+  //   manager.joinRoom(roomId, userId);
+  // });
 
-  socket.on(SocketEvent.LEAVE_ROOM, () => {
-    const roomId = manager.getRoomId(userId);
-    if (roomId) {
-      const userIds = manager.getUserIds(roomId);
-      manager.leaveRoom(userId);
-      if (userIds.length <= 0) {
-        manager.deleteRoom(roomId);
-      }
-    }
-  });
+  // socket.on(SocketEvent.LEAVE_ROOM, () => {
+  //   const roomId = manager.getRoomId(userId);
+  //   if (roomId) {
+  //     const userIds = manager.getUserIds(roomId);
+  //     manager.leaveRoom(userId);
+  //     if (userIds.length <= 0) {
+  //       manager.deleteRoom(roomId);
+  //     }
+  //   }
+  // });
 
   // socket.on(SocketEvent.DATA, (id: string, key: string, data: unknown) => {
   //   socket.to(id).emit(SocketEvent.DATA, key, data);
